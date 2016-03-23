5ce4dedd977161782d119e63a7ab63c736fd8117
vitor <vitormonteirodebarros@gmail.com>
2016-03-23 15:36:07 -0300

    arrumando o config do modulo

--- config/module.config.php
+++ config/module.config.php
@@ -1,22 +1,4 @@
 <?php
 namespace VMBPayPal;
 
-return array(
-    'doctrine'  =>  array(
-        'driver'    =>  array(
-            __NAMESPACE__ . '_driver'  =>  array(
-                'class' =>  'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
-                'cache' =>  'array',
-                'paths' =>  array(__DIR__ . '/../src/' . __NAMESPACE__ . '/Entity'),
-            ),
-            'orm_default'   =>  array(
-                'drivers'   =>  array(
-                    __NAMESPACE__ . '\Entity'   =>  __NAMESPACE__ . '_driver',
-                ),
-            ),
-        ),
-    ),
-    'data-fixture' => array(
-        'UNIUSBase_fixture' => __DIR__ . '/../src/UNIUSBase/Fixture',
-    ),
-);
+return array();
